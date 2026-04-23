322986ee5101c25784ec8a7734a5ef65b7fff83c
Binozo <entwickler@binozoworks.de>
2026-04-23 16:42:05 +0200

    feat(index): Added ITrainableIndex

---
+++ src/FaissNET.Core/Interfaces/ITrainableIndex.cs
@@ -0,0 +1,17 @@
+namespace Faiss.Interfaces;
+
+/// <inheritdoc/>
+public interface ITrainableIndex : IIndex
+{ 
+    /// <summary>
+    /// Value indicating whether the index requires training or is already trained.
+    /// </summary>
+    bool IsTrained { get; }
+    
+    /// <summary>
+    /// Trains the index using a representative sample of your dataset which can take a while.
+    /// </summary>
+    /// <param name="count">Number of training vectors (should be at least 10x the number of centroids).</param>
+    /// <param name="vectors">The sample vectors to learn from.</param>
+    Task TrainAsync(long count, ReadOnlyMemory<float> vectors);
+}
