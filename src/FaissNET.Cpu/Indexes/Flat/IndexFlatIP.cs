213f07f9bc8f0fdc4f395257857607ed8f395024
Binozo <entwickler@binozoworks.de>
2026-04-24 16:47:18 +0200

    faet(index): Added index flat ip

---
+++ src/FaissNET.Cpu/Indexes/Flat/IndexFlatIP.cs
@@ -0,0 +1,30 @@
+using Faiss.Exceptions;
+using Faiss.Interop.Errors;
+
+namespace Faiss.Cpu.Indexes;
+
+using Interfaces;
+using Interop.NativeMethods;
+
+/// <summary>
+/// Exact search for Inner Product (useful for Cosine Similarity).
+/// Ideal for NLP and embedding-based search.
+/// </summary>
+public sealed class IndexFlatIP : CpuIndex<IndexFlatIP>, IFromNativeHandle<IndexFlatIP>, IFlatIndex
+{
+    /// <param name="dimensions">The number of dimensions for vectors in this index.</param>
+    /// <exception cref="FaissException">Thrown when the index creation fails.</exception>
+    public IndexFlatIP(long dimensions)
+    {
+        FaissErrorHandler.ThrowIfError(Native.faiss_IndexFlatIP_new_with(out var handle, dimensions));
+
+        SafeHandle = handle;
+    }
+
+    private IndexFlatIP(IntPtr handle) : base(handle)
+    {
+
+    }
+
+    static IndexFlatIP IFromNativeHandle<IndexFlatIP>.FromHandle(IntPtr handle) => new(handle);
+}
