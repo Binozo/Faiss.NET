708fdea0b53cbb29e7acb716c191974d7a6b9544
Binozo <entwickler@binozoworks.de>
2026-04-24 16:41:54 +0200

    feat(factory): Added Binary Index factory

---
+++ src/FaissNET.Cpu/Factory/BinaryIndexFactory.cs
@@ -0,0 +1,23 @@
+using Faiss.Cpu.Interfaces;
+using Faiss.Interop.Errors;
+using Faiss.Interop.NativeMethods;
+
+namespace Faiss.Cpu.Factory;
+
+public static class BinaryIndexFactory
+{
+    /// <summary>
+    /// Creates a binary index from a string descriptor.
+    /// </summary>
+    /// <param name="description">Factory string (e.g., "BFlat", "BIVF256", "BHNSW32", "BHash16").</param>
+    /// <param name="dimensions">Vector dimensionality in bits.</param>
+    public static T Create<T>(string description, int dimensions)
+        where T : INativeBinaryIndex<T>, IFromNativeBinaryHandle<T>
+    {
+        FaissErrorHandler.ThrowIfError(
+            Native.faiss_index_binary_factory(out IntPtr ptr, dimensions, description)
+        );
+
+        return T.FromHandle(ptr);
+    }
+}
