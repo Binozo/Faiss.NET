36e56d0f8c6bb181f5aa9e45a5aa88e5019a29e9
Binozo <entwickler@binozoworks.de>
2026-04-24 16:45:04 +0200

    feat(index): Added binary index hash

---
+++ src/FaissNET.Cpu/Indexes/Binary/IndexBinaryHash.cs
@@ -0,0 +1,25 @@
+using Faiss.Cpu.Interfaces;
+using Faiss.Interop.Errors;
+using Faiss.Interop.NativeMethods;
+using Faiss.Interop.SafeHandles;
+
+namespace Faiss.Cpu.Indexes.Binary;
+
+public sealed class IndexBinaryHash : BinaryIndex<IndexBinaryHash>, IFromNativeBinaryHandle<IndexBinaryHash>
+{
+    public IndexBinaryHash(int dimensions, int b)
+    {
+        string description = $"BHash{b}";
+        FaissErrorHandler.ThrowIfError(
+            Native.faiss_index_binary_factory(out IntPtr ptr, dimensions, description)
+        );
+
+        SafeHandle = new FaissIndexBinaryHandle(ptr);
+    }
+
+    private IndexBinaryHash(IntPtr handle) : base(handle)
+    {
+    }
+
+    static IndexBinaryHash IFromNativeBinaryHandle<IndexBinaryHash>.FromHandle(IntPtr handle) => new(handle);
+}
