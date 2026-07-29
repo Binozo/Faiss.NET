6a5c9f132f1087d3adf9db1ba783eb1376db6ecb
Binozo <entwickler@binozoworks.de>
2026-07-29 16:19:32 +0200

    feat(Index): Added new FloatIndex base

---
+++ src/FaissNET.Cpu/Indexes/FloatIndex.cs
@@ -0,0 +1,73 @@
+using Faiss.Cpu.Interfaces;
+using Faiss.Interfaces;
+using Faiss.Interop.Errors;
+using Faiss.Interop.NativeMethods;
+using Faiss.Interop.SafeHandles;
+using Faiss.Models;
+
+namespace Faiss.Cpu.Indexes;
+
+/// <inheritdoc cref="IFloatIndex" />
+public abstract class FloatIndex : IFloatIndex, INativeIndex
+{
+    private readonly FaissIndexHandle _handle;
+    
+    protected FloatIndex(FaissIndexHandle handle) => _handle = handle ?? throw new ArgumentNullException(nameof(handle));
+
+    protected internal FaissIndexHandle NativeHandle => _handle;
+
+    FaissIndexHandle INativeIndex.Handle => _handle;
+
+    public int Dimensions => Native.faiss_Index_d(NativeHandle);
+    public long TotalCount => Native.faiss_Index_ntotal(NativeHandle);
+
+    public MetricType Metric => Native.faiss_Index_metric_type(NativeHandle);
+
+    public virtual unsafe void Search(long count, ReadOnlySpan<float> queryVectors, int k, Span<float> distances, Span<long> labels)
+    {
+        fixed (float* pQuery = queryVectors)
+        fixed (float* pDistances = distances)
+        fixed (long* pLabels = labels)
+        {
+            FaissErrorHandler.ThrowIfError(Native.faiss_Index_search(NativeHandle, count, pQuery, k, pDistances, pLabels));
+        }
+    }
+
+    public void Assign(long count, ReadOnlySpan<float> queryVectors, long k, Span<long> labels)
+    {
+        unsafe
+        {
+            fixed (float* pQuery = queryVectors)
+            fixed (long* pLabels = labels)
+            {
+                FaissErrorHandler.ThrowIfError(
+                    Native.faiss_Index_assign(NativeHandle, count, pQuery, pLabels, k)
+                );
+            }
+        }
+    }
+
+    public void SearchWithParams(long count, ReadOnlySpan<float> queryVectors, int k, ISearchParameters parameters, Span<float> distances, Span<long> labels)
+    {
+        unsafe
+        {
+            fixed (float* pQuery = queryVectors)
+            fixed (float* pDistances = distances)
+            fixed (long* pLabels = labels)
+            {
+                FaissErrorHandler.ThrowIfError( // TODO: Improve this INativeSearchParameters handling
+                    Native.faiss_Index_search_with_params(NativeHandle, count, pQuery, k, ((INativeSearchParameters)parameters).Handle, pDistances, pLabels)
+                );
+            }
+        }
+    }
+
+    public void Reset() => FaissErrorHandler.ThrowIfError(Native.faiss_Index_reset(NativeHandle));
+
+
+    public virtual void Dispose()
+    {
+        _handle.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
--- src/FaissNET.Cpu/Interfaces/INativeIndex.cs
+++ src/FaissNET.Cpu/Interfaces/INativeIndex.cs
@@ -3,19 +3,48 @@ using Faiss.Interop.SafeHandles;
 
 namespace Faiss.Cpu.Interfaces;
 
-/// <inheritdoc/>
-public interface INativeIndex : IIndex
+public interface INativeObject
 {
-    protected internal FaissIndexHandle Handle { get; }
+    protected internal FaissHandle Handle { get; }
 }
 
-public interface INativeIndex<TSelf> : INativeIndex
-    where TSelf : INativeIndex<TSelf>
+public interface INativeObject<out T> : INativeObject where T : FaissHandle
 {
+    protected internal new T Handle { get; }
+
+    FaissHandle INativeObject.Handle => Handle;
 }
 
-public interface IFromNativeHandle<TSelf>
-    where TSelf : INativeIndex<TSelf>
+/// <inheritdoc cref="IIndex" />
+public interface INativeIndex : IIndex, INativeObject<FaissIndexHandle>
 {
-    internal static abstract TSelf FromHandle(IntPtr handle);
+    protected internal new FaissIndexHandle Handle { get; }
+
+    FaissIndexHandle INativeObject<FaissIndexHandle>.Handle => Handle;
+}
+
+/// <inheritdoc cref="IIndex" />
+public interface INativeBinaryIndex : IIndex, INativeObject<FaissBinaryIndexHandle>
+{
+    protected internal new FaissBinaryIndexHandle Handle { get; }
+
+    FaissBinaryIndexHandle INativeObject<FaissBinaryIndexHandle>.Handle => Handle;
+}
+
+public interface IFromNativeIndexHandle<T> where T : IFromNativeIndexHandle<T>
+{
+    [Obsolete("Use FromPointer instead")]
+    internal static abstract T FromHandle(FaissIndexHandle handle);
+
+    internal static virtual T FromPointer(IntPtr ptr, bool ownsHandle = true)
+        => T.FromHandle(new FaissIndexHandle(ptr, ownsHandle));
+}
+
+public interface IFromNativeBinaryIndexHandle<T> where T : IFromNativeBinaryIndexHandle<T>
+{
+    [Obsolete("Use FromPointer instead")]
+    internal static abstract T FromHandle(FaissBinaryIndexHandle handle);
+
+    internal static virtual T FromPointer(IntPtr handle, bool ownsHandle = true)
+        => T.FromHandle(new FaissBinaryIndexHandle(handle, ownsHandle));
 }
