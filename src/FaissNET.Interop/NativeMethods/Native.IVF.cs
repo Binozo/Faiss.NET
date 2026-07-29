971edf73f4fd7327a66339465ac5ed15fecd80a6
Binozo <entwickler@binozoworks.de>
2026-07-29 16:17:59 +0200

    fix(native): Improved IVF functions

--- src/FaissNET.Interop/NativeMethods/Native.IVF.cs
+++ src/FaissNET.Interop/NativeMethods/Native.IVF.cs
@@ -16,7 +16,7 @@ internal static unsafe partial class Native
     internal static partial int faiss_IndexIVF_search_preassigned(FaissIndexHandle index, long n, IntPtr x, long k, IntPtr assign, IntPtr centroidDis, IntPtr distances, IntPtr labels, [MarshalAs(UnmanagedType.Bool)] bool storePairs);
     
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexIVF_get_list_size(FaissIndexHandle index, UIntPtr listNo);
+    internal static partial nuint faiss_IndexIVF_get_list_size(FaissIndexHandle index, nuint listNo);
     
     [LibraryImport(LibraryName)]
     internal static partial int faiss_IndexIVF_make_direct_map(FaissIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool maintainDirectMap);
@@ -28,25 +28,25 @@ internal static unsafe partial class Native
     internal static partial void faiss_IndexIVF_print_stats(FaissIndexHandle index);
     
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexIVF_invlists_get_ids(FaissIndexHandle index, UIntPtr listNo, IntPtr invlist);
+    internal static partial void faiss_IndexIVF_invlists_get_ids(FaissIndexHandle index, nuint listNo, IntPtr invlist);
     
     [LibraryImport(LibraryName)]
     internal static partial int faiss_IndexIVF_train_encoder(FaissIndexHandle index, long n, IntPtr x, IntPtr assign);
     
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexIVFScalarQuantizer_new_with(out IntPtr pIndex, FaissIndexHandle quantizer, long d, UIntPtr nlist, QuantizerType qt);
+    internal static partial int faiss_IndexIVFScalarQuantizer_new_with(out IntPtr pIndex, FaissIndexHandle quantizer, long d, nuint nlist, QuantizerType qt);
     
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexIVFScalarQuantizer_new_with_metric(out IntPtr pIndex, FaissIndexHandle quantizer, UIntPtr d, UIntPtr nlist, QuantizerType qt, MetricType metric, [MarshalAs(UnmanagedType.Bool)] bool encodeResidual);
+    internal static partial int faiss_IndexIVFScalarQuantizer_new_with_metric(out IntPtr pIndex, FaissIndexHandle quantizer, nuint d, nuint nlist, QuantizerType qt, MetricType metric, [MarshalAs(UnmanagedType.Bool)] bool encodeResidual);
     
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexIVFScalarQuantizer_nlist(FaissIndexHandle index);
+    internal static partial nuint faiss_IndexIVFScalarQuantizer_nlist(FaissIndexHandle index);
     
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexIVFScalarQuantizer_nprobe(FaissIndexHandle index);
+    internal static partial nuint faiss_IndexIVFScalarQuantizer_nprobe(FaissIndexHandle index);
     
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexIVFScalarQuantizer_set_nprobe(FaissIndexHandle index, UIntPtr nprobe);
+    internal static partial void faiss_IndexIVFScalarQuantizer_set_nprobe(FaissIndexHandle index, nuint nprobe);
     
     [LibraryImport(LibraryName)]
     internal static partial IntPtr faiss_IndexIVFScalarQuantizer_quantizer(FaissIndexHandle index);
@@ -67,13 +67,13 @@ internal static unsafe partial class Native
     internal static partial int faiss_IndexIVFFlat_new_with_metric(out IntPtr pIndex, FaissIndexHandle quantizer, UIntPtr d, UIntPtr nlist, MetricType metric);
     
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexIVFFlat_nlist(FaissIndexHandle index);
+    internal static partial nuint faiss_IndexIVFFlat_nlist(FaissIndexHandle index);
     
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexIVFFlat_nprobe(FaissIndexHandle index);
+    internal static partial nuint faiss_IndexIVFFlat_nprobe(FaissIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexIVFFlat_set_nprobe(FaissIndexHandle index, UIntPtr nprobe);
+    internal static partial void faiss_IndexIVFFlat_set_nprobe(FaissIndexHandle index, nuint nprobe);
     
     [LibraryImport(LibraryName)]
     internal static partial IntPtr faiss_IndexIVFFlat_quantizer(FaissIndexHandle index);
