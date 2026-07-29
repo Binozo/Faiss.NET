0620f7e0ec472a39d0be1170d5da615626bf167e
Binozo <entwickler@binozoworks.de>
2026-07-29 16:16:45 +0200

    feat(native): Added more flat operations

--- src/FaissNET.Interop/NativeMethods/Native.Flat.cs
+++ src/FaissNET.Interop/NativeMethods/Native.Flat.cs
@@ -7,6 +7,11 @@ namespace Faiss.Interop.NativeMethods;
 internal static unsafe partial class Native
 {
     [LibraryImport(LibraryName)]
+    internal static partial int faiss_IndexFlat_new(out IntPtr p_index);
+    [LibraryImport(LibraryName)]
+    internal static partial int faiss_IndexFlat_new_with(out IntPtr p_index, long d, MetricType metric);
+
+    [LibraryImport(LibraryName)]
     internal static partial int faiss_IndexFlat1D_new(out IntPtr pIndex);
     [LibraryImport(LibraryName)]
     internal static partial int faiss_IndexFlat1D_new_with(out IntPtr pIndex, [MarshalAs(UnmanagedType.Bool)] bool continuousUpdate);
@@ -14,16 +19,19 @@ internal static unsafe partial class Native
     internal static partial int faiss_IndexFlat1D_update_permutation(FaissIndexHandle index);
     
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexFlatL2_new(out FaissIndexHandle p_index);
+    internal static partial int faiss_IndexFlatL2_new(out IntPtr p_index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexFlatL2_new_with(out FaissIndexHandle p_index, long d);
+    internal static partial int faiss_IndexFlatL2_new_with(out IntPtr p_index, long d);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexFlatIP_new(out FaissIndexHandle p_index);
+    internal static partial int faiss_IndexFlatIP_new(out IntPtr p_index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexFlatIP_new_with(out FaissIndexHandle p_index, long d);
+    internal static partial int faiss_IndexFlatIP_new_with(out IntPtr p_index, long d);
+
+    [LibraryImport(LibraryName)]
+    internal static partial void faiss_IndexFlatIP_free(IntPtr p_index);
     
     [LibraryImport(LibraryName)]
     internal static partial int faiss_IndexScalarQuantizer_new_with(out IntPtr pIndex, long d, QuantizerType qt, MetricType metric);
