43104bab0c97720f37f775dd7eedf877b265a78b
Binozo <entwickler@binozoworks.de>
2026-08-05 16:42:35 +0200

    feat(index): Completed IndexBinaryIDMap

--- src/FaissNET.Cpu/Indexes/Binary/IndexBinaryIDMap.cs
+++ src/FaissNET.Cpu/Indexes/Binary/IndexBinaryIDMap.cs
@@ -1,6 +1,20 @@
+using Faiss.Cpu.Factory;
+using Faiss.Cpu.Interfaces;
+using Faiss.Interop.SafeHandles;
+
 namespace Faiss.Cpu.Indexes.Binary;
 
-public class IndexBinaryIDMap
+public sealed class IndexBinaryIDMap<T> : MappedBinaryIndex<IndexBinaryIDMap<T>, T>, IFromNativeBinaryIndexHandle<IndexBinaryIDMap<T>> where T : IIDSequentialBinaryIndex, IBinaryIndex, IFromNativeBinaryIndexHandle<T>
 {
-    
+    public IndexBinaryIDMap(string description, int dimension) : this(CreateHandle($"IDMap,{description}", dimension))
+    {
+    }
+
+    private IndexBinaryIDMap(FaissBinaryIndexHandle handle) : base(handle)
+    {
+    }
+
+    private static FaissBinaryIndexHandle CreateHandle(string description, int dimension) => BinaryIndexFactory.Create<IndexBinaryIDMap<T>>(description, dimension).NativeHandle;
+
+    static IndexBinaryIDMap<T> IFromNativeBinaryIndexHandle<IndexBinaryIDMap<T>>.FromHandle(FaissBinaryIndexHandle handle) => new(handle);
 }
