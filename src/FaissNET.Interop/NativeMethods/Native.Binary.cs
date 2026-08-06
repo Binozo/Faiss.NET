e7c24ed6469e96f68cbdee9aabf43742182fcec7
Binozo <entwickler@binozoworks.de>
2026-08-06 09:57:09 +0200

    feat(native): Added/Adjusted binary bindings

--- src/FaissNET.Interop/NativeMethods/Native.Binary.cs
+++ src/FaissNET.Interop/NativeMethods/Native.Binary.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using Faiss.Cpu.Search;
 using Faiss.Interop.SafeHandles;
 using Faiss.Models;
 
@@ -25,110 +26,116 @@ internal static unsafe partial class Native
     internal static partial int faiss_IndexLSH_train_thresholds(FaissIndexHandle index);
 
     [LibraryImport(LibraryName)]
+    internal static partial int faiss_IndexLSH_free(IntPtr p_index);
+
+    [LibraryImport(LibraryName)]
     internal static partial void faiss_IndexBinary_free(IntPtr index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_d(FaissIndexBinaryHandle index);
+    internal static partial int faiss_IndexBinary_d(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_is_trained(FaissIndexBinaryHandle index);
+    internal static partial int faiss_IndexBinary_is_trained(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial long faiss_IndexBinary_ntotal(FaissIndexBinaryHandle index);
+    internal static partial long faiss_IndexBinary_ntotal(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial MetricType faiss_IndexBinary_metric_type(FaissIndexBinaryHandle index);
+    internal static partial MetricType faiss_IndexBinary_metric_type(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_verbose(FaissIndexBinaryHandle index);
+    internal static partial int faiss_IndexBinary_verbose(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinary_set_verbose(FaissIndexBinaryHandle index, [MarshalAs(UnmanagedType.Bool)] bool verbose);
+    internal static partial void faiss_IndexBinary_set_verbose(FaissBinaryIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool verbose);
+
+    [LibraryImport(LibraryName)] // TODO: Convert the others too!
+    internal static partial int faiss_IndexBinary_train(FaissBinaryIndexHandle index, long n, byte* x);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_train(FaissIndexBinaryHandle index, long n, byte* x);
+    internal static partial int faiss_IndexBinary_add(FaissBinaryIndexHandle index, long n, byte* x);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_add(FaissIndexBinaryHandle index, long n, byte* x);
+    internal static partial int faiss_IndexBinary_add_with_ids(FaissBinaryIndexHandle index, long n, byte* x, long* xids);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_add_with_ids(FaissIndexBinaryHandle index, long n, byte* x, long* xids);
+    internal static partial int faiss_IndexBinary_search(FaissBinaryIndexHandle index, long n, byte* x, long k, int* distances, long* labels);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_search(FaissIndexBinaryHandle index, long n, byte* x, long k, int* distances, long* labels);
+    internal static partial int faiss_IndexBinary_search_with_params(FaissBinaryIndexHandle index, long n, byte* x, long k, FaissSearchParametersHandle @params, int* distances, long* labels);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_search_with_params(FaissIndexBinaryHandle index, long n, byte* x, long k, IntPtr @params, int* distances, long* labels);
+    internal static partial int faiss_IndexBinary_range_search(FaissBinaryIndexHandle index, long n, byte* x, int radius, FaissRangeSearchResultHandle result);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_range_search(FaissIndexBinaryHandle index, long n, byte* x, int radius, IntPtr result);
+    internal static partial int faiss_IndexBinary_assign(FaissBinaryIndexHandle index, long n, byte* x, long* labels, long k);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_assign(FaissIndexBinaryHandle index, long n, byte* x, long* labels, long k);
+    internal static partial int faiss_IndexBinary_reset(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_reset(FaissIndexBinaryHandle index);
+    internal static partial int faiss_IndexBinary_remove_ids(FaissBinaryIndexHandle index, IntPtr sel, out nuint nRemoved);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_remove_ids(FaissIndexBinaryHandle index, IntPtr sel, out UIntPtr nRemoved);
+    internal static partial int faiss_IndexBinary_reconstruct(FaissBinaryIndexHandle index, long key, byte* recons);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_reconstruct(FaissIndexBinaryHandle index, long key, byte* recons);
+    internal static partial int faiss_IndexBinary_reconstruct_n(FaissBinaryIndexHandle index, long i0, long ni, byte* recons);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinary_reconstruct_n(FaissIndexBinaryHandle index, long i0, long ni, byte* recons);
+    internal static partial int faiss_clone_index_binary(FaissBinaryIndexHandle index, out IntPtr p_out);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_clone_index_binary(FaissIndexBinaryHandle index, out IntPtr p_out);
+    internal static partial UIntPtr faiss_IndexBinaryIVF_nlist(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexBinaryIVF_nlist(FaissIndexBinaryHandle index);
+    internal static partial UIntPtr faiss_IndexBinaryIVF_nprobe(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexBinaryIVF_nprobe(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_set_nprobe(FaissBinaryIndexHandle index, nuint nprobe);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinaryIVF_set_nprobe(FaissIndexBinaryHandle index, UIntPtr nprobe);
+    internal static partial IntPtr faiss_IndexBinaryIVF_quantizer(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial IntPtr faiss_IndexBinaryIVF_quantizer(FaissIndexBinaryHandle index);
+    internal static partial int faiss_IndexBinaryIVF_own_fields(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinaryIVF_own_fields(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_set_own_fields(FaissBinaryIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool ownFields);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinaryIVF_set_own_fields(FaissIndexBinaryHandle index, [MarshalAs(UnmanagedType.Bool)] bool ownFields);
+    internal static partial UIntPtr faiss_IndexBinaryIVF_max_codes(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexBinaryIVF_max_codes(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_set_max_codes(FaissBinaryIndexHandle index, nuint maxCodes);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinaryIVF_set_max_codes(FaissIndexBinaryHandle index, UIntPtr maxCodes);
+    internal static partial int faiss_IndexBinaryIVF_use_heap(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinaryIVF_use_heap(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_set_use_heap(FaissBinaryIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool useHeap);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinaryIVF_set_use_heap(FaissIndexBinaryHandle index, [MarshalAs(UnmanagedType.Bool)] bool useHeap);
+    internal static partial int faiss_IndexBinaryIVF_per_invlist_search(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinaryIVF_per_invlist_search(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_set_per_invlist_search(FaissBinaryIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool perInvlistSearch);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinaryIVF_set_per_invlist_search(FaissIndexBinaryHandle index, [MarshalAs(UnmanagedType.Bool)] bool perInvlistSearch);
+    internal static partial int faiss_IndexBinaryIVF_merge_from(FaissBinaryIndexHandle index, IntPtr other, long addId);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinaryIVF_merge_from(FaissIndexBinaryHandle index, IntPtr other, long addId);
+    internal static partial UIntPtr faiss_IndexBinaryIVF_get_list_size(FaissBinaryIndexHandle index, nuint listNo);
 
     [LibraryImport(LibraryName)]
-    internal static partial UIntPtr faiss_IndexBinaryIVF_get_list_size(FaissIndexBinaryHandle index, UIntPtr listNo);
+    internal static partial int faiss_IndexBinaryIVF_make_direct_map(FaissBinaryIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool maintainDirectMap);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexBinaryIVF_make_direct_map(FaissIndexBinaryHandle index, [MarshalAs(UnmanagedType.Bool)] bool maintainDirectMap);
+    internal static partial double faiss_IndexBinaryIVF_imbalance_factor(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial double faiss_IndexBinaryIVF_imbalance_factor(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_print_stats(FaissBinaryIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_IndexBinaryIVF_print_stats(FaissIndexBinaryHandle index);
+    internal static partial void faiss_IndexBinaryIVF_free(IntPtr p_index);
 }
