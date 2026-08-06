d3be3300dc6600025ebac7a3581bf50872e0e615
Binozo <entwickler@binozoworks.de>
2026-08-06 10:02:09 +0200

    fix(serializer): Use ICpuFloatIndex instead of INativeIndex to ensure index is on cpu

--- src/FaissNET.Cpu/Serializer/IndexSerializer.cs
+++ src/FaissNET.Cpu/Serializer/IndexSerializer.cs
@@ -10,14 +10,14 @@ namespace Faiss.Cpu.Serializer;
 /// </summary>
 public static class IndexSerializer
 {
-    public static void Write(INativeIndex index, string filePath)
+    public static void Write(ICpuFloatIndex index, string filePath)
     {
         FaissErrorHandler.ThrowIfError(
             Native.faiss_write_index_fname(index.Handle, filePath)
         );
     }
     
-    public static void Write(INativeIndex index, Stream stream)
+    public static void Write(ICpuFloatIndex index, Stream stream)
     {
         Native.CustomIoWriterCallback writerCallback = (ptr, size, nitems) =>
         {
