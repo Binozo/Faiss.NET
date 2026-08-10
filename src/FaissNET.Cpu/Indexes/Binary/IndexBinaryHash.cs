ddd2f550e1af7717d17e7617914e5123c441eb95
Binozo <entwickler@binozoworks.de>
2026-08-10 15:51:04 +0200

    feat(index): Added IndexBinaryHash

--- src/FaissNET.Cpu/Indexes/Binary/IndexBinaryHash.cs
+++ src/FaissNET.Cpu/Indexes/Binary/IndexBinaryHash.cs
@@ -1,25 +1,42 @@
 using Faiss.Cpu.Interfaces;
+using Faiss.Cpu.Search.Range;
+using Faiss.Interfaces;
 using Faiss.Interop.Errors;
 using Faiss.Interop.NativeMethods;
 using Faiss.Interop.SafeHandles;
 
 namespace Faiss.Cpu.Indexes.Binary;
 
-public sealed class IndexBinaryHash : CpuBinaryIndex<IndexBinaryHash>, IFromNativeBinaryHandle<IndexBinaryHash>
+public sealed class IndexBinaryHash : BinaryIndex, IIDSequentialBinaryIndex, IIDMappedBinaryIndex, IParamsBinarySearchIndex, IRangeSearchBinaryIndex, IClonableBinaryIndex<IndexBinaryHash>, IFromNativeBinaryIndexHandle<IndexBinaryHash>
 {
-    public IndexBinaryHash(int dimensions, int b)
-    {
-        string description = $"BHash{b}";
-        FaissErrorHandler.ThrowIfError(
-            Native.faiss_index_binary_factory(out IntPtr ptr, dimensions, description)
-        );
-
-        SafeHandle = new FaissIndexBinaryHandle(ptr);
-    }
-
-    private IndexBinaryHash(IntPtr handle) : base(handle)
+    public IndexBinaryHash(int dimensions, int leadingBits) : this(CreateHandle(dimensions, leadingBits))
     {
     }
 
-    static IndexBinaryHash IFromNativeBinaryHandle<IndexBinaryHash>.FromHandle(IntPtr handle) => new(handle);
+    private IndexBinaryHash(FaissBinaryIndexHandle handle) : base(handle)
+    {
+    }
+
+    private static FaissBinaryIndexHandle CreateHandle(int dimensions, int leadingBits)
+    {
+        if (dimensions <= 0 || dimensions % 8 != 0)
+        {
+            throw new ArgumentException("Dimensions must be divisible by 8", nameof(dimensions));
+        }
+
+        FaissErrorHandler.ThrowIfError(Native.faiss_index_binary_factory(out IntPtr ptr, dimensions, $"BHash{leadingBits}"));
+        return new FaissBinaryIndexHandle(ptr);
+    }
+
+    static IndexBinaryHash IFromNativeBinaryIndexHandle<IndexBinaryHash>.FromHandle(FaissBinaryIndexHandle handle) => new(handle);
+
+    public void Add(long count, ReadOnlySpan<byte> vectors) => ((IIDSequentialBinaryIndex)this).Add(count, vectors);
+
+    public void Add(long count, ReadOnlySpan<byte> vectors, ReadOnlySpan<long> xids) => ((IIDMappedBinaryIndex)this).Add(count, vectors, xids);
+
+    public void RangeSearch(long count, ReadOnlySpan<byte> queryVectors, byte radius, RangeSearchResult result) => ((IRangeSearchBinaryIndex)this).RangeSearch(count, queryVectors, radius, result);
+
+    public void SearchWithParams(long count, ReadOnlySpan<byte> queryVectors, int k, ISearchParameters parameters, Span<int> distances, Span<long> labels) => ((IParamsBinarySearchIndex)this).SearchWithParams(count, queryVectors, k, parameters, distances, labels);
+
+    public IndexBinaryHash Clone() => ((IClonableBinaryIndex<IndexBinaryHash>)this).Clone();
 }
