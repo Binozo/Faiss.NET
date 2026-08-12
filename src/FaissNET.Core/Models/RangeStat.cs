a546a506539bc267d7d4a826221b24d64f1b6dde
Binozo <entwickler@binozoworks.de>
2026-08-12 10:39:46 +0200

    feat(RangeStat): Added enum

--- src/FaissNET.Core/Models/RangeStat.cs
+++ src/FaissNET.Core/Models/RangeStat.cs
@@ -1,6 +1,9 @@
 namespace Faiss.Models;
 
-public class RangeStat
+public enum RangeStat
 {
-    
+    RS_minmax = 0,    ///< [min - rs*(max-min), max + rs*(max-min)]
+    RS_meanstd = 1,   ///< [mean - std * rs, mean + std * rs]
+    RS_quantiles = 2, ///< [Q(rs), Q(1-rs)]
+    RS_optim = 3      ///< alternate optimization of reconstruction error
 }
