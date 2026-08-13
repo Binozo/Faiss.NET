2d87108d6a9dbc0a9e32b9ccadc97c146c74c1fd
Binozo <entwickler@binozoworks.de>
2026-08-13 09:33:56 +0200

    fix(native): Use dedicated FaissVectorTransformHandle for transform functions

--- src/FaissNET.Interop/NativeMethods/Native.Transform.cs
+++ src/FaissNET.Interop/NativeMethods/Native.Transform.cs
@@ -36,16 +36,16 @@ internal static unsafe partial class Native
     internal static partial int faiss_PCAMatrix_new_with(out IntPtr pVt, int dIn, int dOut, float eigenPower, [MarshalAs(UnmanagedType.Bool)] bool randomRotation);
 
     [LibraryImport(LibraryName)]
-    internal static partial float faiss_PCAMatrix_eigen_power(IntPtr vt);
+    internal static partial float faiss_PCAMatrix_eigen_power(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_PCAMatrix_random_rotation(IntPtr vt);
+    internal static partial int faiss_PCAMatrix_random_rotation(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_PCAMatrix_balanced_bins(IntPtr vt);
+    internal static partial int faiss_PCAMatrix_balanced_bins(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_PCAMatrix_set_balanced_bins(IntPtr vt, int balancedBins);
+    internal static partial void faiss_PCAMatrix_set_balanced_bins(FaissVectorTransformHandle vt, int balancedBins);
 
     [LibraryImport(LibraryName)]
     internal static partial int faiss_ITQMatrix_new_with(out IntPtr pVt, int d);
@@ -54,28 +54,28 @@ internal static unsafe partial class Native
     internal static partial int faiss_ITQTransform_new_with(out IntPtr pVt, int dIn, int dOut, [MarshalAs(UnmanagedType.Bool)] bool doPca);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_ITQTransform_do_pca(IntPtr vt);
+    internal static partial int faiss_ITQTransform_do_pca(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
     internal static partial int faiss_OPQMatrix_new_with(out IntPtr pVt, int d, int m, int d2);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_OPQMatrix_verbose(IntPtr vt);
+    internal static partial int faiss_OPQMatrix_verbose(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_OPQMatrix_set_verbose(IntPtr vt, [MarshalAs(UnmanagedType.Bool)] bool verbose);
+    internal static partial void faiss_OPQMatrix_set_verbose(FaissVectorTransformHandle vt, [MarshalAs(UnmanagedType.Bool)] bool verbose);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_OPQMatrix_niter(IntPtr vt);
+    internal static partial int faiss_OPQMatrix_niter(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_OPQMatrix_set_niter(IntPtr vt, int niter);
+    internal static partial void faiss_OPQMatrix_set_niter(FaissVectorTransformHandle vt, int niter);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_OPQMatrix_niter_pq(IntPtr vt);
+    internal static partial int faiss_OPQMatrix_niter_pq(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
-    internal static partial void faiss_OPQMatrix_set_niter_pq(IntPtr vt, int niterPq);
+    internal static partial void faiss_OPQMatrix_set_niter_pq(FaissVectorTransformHandle vt, int niterPq);
 
     [LibraryImport(LibraryName)]
     internal static partial int faiss_RemapDimensionsTransform_new_with(out IntPtr pVt, int dIn, int dOut, [MarshalAs(UnmanagedType.Bool)] bool uniform);
@@ -84,16 +84,19 @@ internal static unsafe partial class Native
     internal static partial int faiss_NormalizationTransform_new_with(out IntPtr pVt, int d, float norm);
 
     [LibraryImport(LibraryName)]
-    internal static partial float faiss_NormalizationTransform_norm(IntPtr vt);
+    internal static partial float faiss_NormalizationTransform_norm(FaissVectorTransformHandle vt);
 
     [LibraryImport(LibraryName)]
     internal static partial int faiss_CenteringTransform_new_with(out IntPtr pVt, int d);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexPreTransform_new_with_transform(out IntPtr pIndex, IntPtr ltrans, FaissIndexHandle index);
+    internal static partial int faiss_IndexPreTransform_new_with_transform(out IntPtr pIndex, FaissVectorTransformHandle ltrans, FaissIndexHandle index);
 
     [LibraryImport(LibraryName)]
-    internal static partial int faiss_IndexPreTransform_prepend_transform(FaissIndexHandle index, IntPtr ltrans);
+    internal static partial int faiss_IndexPreTransform_new_with(out IntPtr pIndex, FaissIndexHandle index);
+
+    [LibraryImport(LibraryName)]
+    internal static partial int faiss_IndexPreTransform_prepend_transform(FaissIndexHandle index, FaissVectorTransformHandle ltrans);
 
     [LibraryImport(LibraryName)]
     internal static partial IntPtr faiss_IndexPreTransform_index(FaissIndexHandle index);
@@ -103,4 +106,7 @@ internal static unsafe partial class Native
 
     [LibraryImport(LibraryName)]
     internal static partial void faiss_IndexPreTransform_set_own_fields(FaissIndexHandle index, [MarshalAs(UnmanagedType.Bool)] bool ownFields);
+
+    [LibraryImport(LibraryName)]
+    internal static partial void faiss_IndexPreTransform_free(IntPtr index);
 }
