2a5b58971d9f184611dec1a0b671372c8220e908
Binozo <entwickler@binozoworks.de>
2026-08-28 12:28:25 +0200

    feat(MappedBinaryIndex): Completed meta index

--- src/FaissNET.Cpu/Indexes/Binary/MappedBinaryIndex.cs
+++ src/FaissNET.Cpu/Indexes/Binary/MappedBinaryIndex.cs
@@ -8,9 +8,13 @@ using ITrainableBinaryIndex = Faiss.Cpu.Interfaces.ITrainableBinaryIndex;
 namespace Faiss.Cpu.Indexes.Binary;
 
 /// <inheritdoc cref="MappedBinaryIndex{T, TIndex}"/>
-public class MappedBinaryIndex<T, TIndex> : BinaryIndex, IRangeSearchBinaryIndex, IIDRemovableBinaryIndex, IIDMappedBinaryIndex, ITrainableBinaryIndex, ICpuBinaryIndex, IParamsBinarySearchIndex, IClonableBinaryIndex<T> where T : MappedBinaryIndex<T, TIndex>, IFromNativeBinaryIndexHandle<T> where TIndex : IIDSequentialBinaryIndex, IBinaryIndex, IFromNativeBinaryIndexHandle<TIndex>
+public class MappedBinaryIndex<T, TIndex> : BinaryIndex, IRangeSearchBinaryIndex, IIDRemovableBinaryIndex, IIDMappedBinaryIndex, ITrainableBinaryIndex, ICpuBinaryIndex, IParamsBinarySearchIndex, ISerializableBinaryIndex, IClonableBinaryIndex<T> where T : MappedBinaryIndex<T, TIndex>, IFromNativeBinaryIndexHandle<T> where TIndex : IIDSequentialBinaryIndex, IBinaryIndex, IFromNativeBinaryIndexHandle<TIndex>
 {
     protected MappedBinaryIndex(FaissBinaryIndexHandle handle) : base(handle) {}
+
+    public bool IsTrained => ((ITrainableBinaryIndex)this).IsTrained;
+
+    public Task TrainAsync(long count, ReadOnlyMemory<byte> vectors) =>  ((ITrainableBinaryIndex)this).TrainAsync(count, vectors);
     
     public void Add(long count, ReadOnlySpan<byte> vectors, ReadOnlySpan<long> xids) => ((IIDMappedBinaryIndex)this).Add(count, vectors, xids);
 
@@ -19,10 +23,6 @@ public class MappedBinaryIndex<T, TIndex> : BinaryIndex, IRangeSearchBinaryIndex
     public void SearchWithParams(long count, ReadOnlySpan<byte> queryVectors, int k, ISearchParameters parameters, Span<int> distances, Span<long> labels) => ((IParamsBinarySearchIndex)this).SearchWithParams(count, queryVectors, k, parameters, distances, labels);
 
     public long RemoveIds(IIDSelector selector) => ((IIDRemovableBinaryIndex)this).RemoveIds(selector);
-
-    public new bool IsTrained => ((ITrainableBinaryIndex)this).IsTrained;
-
-    public Task TrainAsync(long count, ReadOnlyMemory<byte> vectors) =>  ((ITrainableBinaryIndex)this).TrainAsync(count, vectors);
 
     public T Clone() => ((IClonableBinaryIndex<T>)this).Clone();
 }
