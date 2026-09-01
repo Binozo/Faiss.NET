bd54a9842185f3986458d88cbee71933d0f231eb
Binozo <entwickler@binozoworks.de>
2026-09-01 17:01:55 +0200

    feat(IDSelectorNot): Added documentation and constructor check

--- src/FaissNET.Cpu/Selectors/IDSelectorNot.cs
+++ src/FaissNET.Cpu/Selectors/IDSelectorNot.cs
@@ -7,6 +7,9 @@ public sealed class IDSelectorNot : IDSelector
 {
     private readonly IDSelector _sel;
     
+    /// <summary>Negation of <paramref name="sel"/>.</summary>
+    /// <param name="sel">Operand. Must remain undisposed for the lifetime of this instance.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="sel"/> is <see langword="null"/>.</exception>
     public IDSelectorNot(IDSelector sel) : base(CreateHandle(sel))
     {
         _sel = sel;
@@ -14,6 +17,8 @@ public sealed class IDSelectorNot : IDSelector
     
     private static IntPtr CreateHandle(IDSelector sel)
     {
+        ArgumentNullException.ThrowIfNull(sel);
+
         FaissErrorHandler.ThrowIfError(
             Native.faiss_IDSelectorNot_new(out IntPtr ptr, sel.SafeHandle)
         );
