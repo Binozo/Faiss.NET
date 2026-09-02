e532cb34cee3df6ba2c8dbbeb952e88d08726934
Binozo <entwickler@binozoworks.de>
2026-09-02 16:02:50 +0200

    fix(FlatFloatIndex): Migrated to new index operations implementations

--- src/FaissNET.Cpu/Indexes/Flat/FlatFloatIndex.cs
+++ src/FaissNET.Cpu/Indexes/Flat/FlatFloatIndex.cs
@@ -1,49 +1,49 @@
 using Faiss.Cpu.Interfaces;
+using Faiss.Cpu.Search.Parameters;
 using Faiss.Cpu.Search.Range;
-using Faiss.Interfaces;
+using Faiss.Cpu.Selectors;
 using Faiss.Interop.SafeHandles;
-using Faiss.Search;
 
 namespace Faiss.Cpu.Indexes.Flat;
 
 /// <inheritdoc cref="FlatFloatIndex{T}" />
-public abstract class FlatFloatIndex<T> : FloatIndex, IFlatIndex, IRangeSearchFloatIndex, IParamsFloatSearchIndex, IIDRemovableFloatIndex, IReconstructFloatIndex, IComputeResidualFloatIndex, ICodeFloatIndex where T : FloatIndex, INativeIndex, IFromNativeIndexHandle<T>
+public abstract class FlatFloatIndex<T> : FloatIndex, IFlatIndex, IParamsFloatSearchIndex, IReconstructFloatIndex, IComputeResidualFloatIndex where T : FloatIndex, INativeIndex, IFromNativeIndexHandle<T>
 {
-    protected FlatFloatIndex(FaissIndexHandle handle) : base(handle) { }
+    internal FlatFloatIndex(FaissIndexHandle handle) : base(handle) { }
     
-    public void SearchWithParams(long count, ReadOnlySpan<float> queryVectors, int k, ISearchParameters parameters, Span<float> distances, Span<long> labels) => ((IParamsFloatSearchIndex)this).SearchWithParams(count, queryVectors, k, parameters, distances, labels);
+    public void SearchWithParams(long count, ReadOnlySpan<float> queryVectors, int k, SearchParameters parameters, Span<float> distances, Span<long> labels) => ParamsFloatSearchIndexImpl.SearchWithParams(this, count, queryVectors, k, parameters, distances, labels);
 
-    public virtual float[] Reconstruct(long key) =>  ((IReconstructFloatIndex)this).Reconstruct(key);
+    public virtual float[] Reconstruct(long key) => ReconstructFloatIndexImpl.Reconstruct(this, key);
 
-    public virtual float[] Reconstruct(long startKey, long count)  => ((IReconstructFloatIndex)this).Reconstruct(startKey, count);
+    public virtual float[] Reconstruct(long startKey, long count) => ReconstructFloatIndexImpl.Reconstruct(this, startKey, count);
 
-    public void ComputeResidual(ReadOnlySpan<float> originalVector, Span<float> residualVector, long key) => ((IComputeResidualFloatIndex)this).ComputeResidual(originalVector, residualVector, key);
+    public void ComputeResidual(ReadOnlySpan<float> originalVector, Span<float> residualVector, long key) => ComputeResidualFloatIndexImpl.ComputeResidual(this, originalVector, residualVector, key);
     
-    public void ComputeResidual(ReadOnlySpan<float> originalVectors, Span<float> residualVectors, ReadOnlySpan<long> keys) => ((IComputeResidualFloatIndex)this).ComputeResidual(originalVectors, residualVectors, keys);
+    public void ComputeResidual(ReadOnlySpan<float> originalVectors, Span<float> residualVectors, ReadOnlySpan<long> keys) => ComputeResidualFloatIndexImpl.ComputeResidual(this, originalVectors, residualVectors, keys);
 }
 
 /// <inheritdoc cref="CpuFlatFloatIndex{T}" />
-public abstract class CpuFlatFloatIndex<T> : FlatFloatIndex<T>, IRangeSearchFloatIndex, IIDRemovableFloatIndex, ICodeFloatIndex, IClonableFloatIndex<T>, ICpuFloatIndex, IIDSequentialFloatIndex where T : FloatIndex, INativeIndex, IFromNativeIndexHandle<T>
+public abstract class CpuFlatFloatIndex<T> : FlatFloatIndex<T>, IIDSequentialFloatIndex, IRangeSearchFloatIndex, IIDRemovableFloatIndex, ICodeFloatIndex, IClonableFloatIndex<T>, ICpuFloatIndex where T : FloatIndex, INativeIndex, IFromNativeIndexHandle<T>
 {
-    protected CpuFlatFloatIndex(FaissIndexHandle handle) : base(handle) { }
+    internal CpuFlatFloatIndex(FaissIndexHandle handle) : base(handle) { }
 
-    public virtual void Add(long count, ReadOnlySpan<float> vectors) => ((IIDSequentialFloatIndex)this).Add(count, vectors);
+    public virtual void Add(long count, ReadOnlySpan<float> vectors) => IDSequentialFloatIndexImpl.Add(this, count, vectors);
 
-    public void RangeSearch(long count, ReadOnlySpan<float> queryVectors, float radius, RangeSearchResult result) => ((IRangeSearchFloatIndex)this).RangeSearch(count, queryVectors, radius, result);
+    public void RangeSearch(long count, ReadOnlySpan<float> queryVectors, float radius, RangeSearchResult result) => RangeSearchFloatIndexImpl.RangeSearch(this, count, queryVectors, radius, result);
     
-    public long RemoveIds(IIDSelector selector) => ((IIDRemovableFloatIndex)this).RemoveIds(selector);
+    public long RemoveIds(IDSelector selector) => IDRemovableFloatIndexImpl.RemoveIds(this, selector);
 
-    public long GetStandaloneCodeSize() => ((ICodeFloatIndex)this).GetStandaloneCodeSize();
+    public long GetStandaloneCodeSize() => CodeFloatIndexImpl.GetStandaloneCodeSize(this);
     
-    public void Encode(long count, ReadOnlySpan<float> vectors, Span<byte> outputBytes)  => ((ICodeFloatIndex)this).Encode(count, vectors, outputBytes);
+    public void Encode(long count, ReadOnlySpan<float> vectors, Span<byte> outputBytes)  => CodeFloatIndexImpl.Encode(this, count, vectors, outputBytes);
     
-    public void Decode(long count, ReadOnlySpan<byte> inputBytes, Span<float> outputVectors)  => ((ICodeFloatIndex)this).Decode(count, inputBytes, outputVectors);
+    public void Decode(long count, ReadOnlySpan<byte> inputBytes, Span<float> outputVectors)  => CodeFloatIndexImpl.Decode(this, count, inputBytes, outputVectors);
     
-    public T Clone() => ((IClonableFloatIndex<T>)this).Clone();
+    public T Clone() => ClonableFloatIndexImpl<T>.Clone(this);
 }
 
 /// <inheritdoc cref="GpuFlatFloatIndex{T}" />
 public abstract class GpuFlatFloatIndex<T> : FlatFloatIndex<T> where T : FloatIndex, INativeIndex, IFromNativeIndexHandle<T>
 {
-    protected GpuFlatFloatIndex(FaissIndexHandle handle) : base(handle) { }
+    internal GpuFlatFloatIndex(FaissIndexHandle handle) : base(handle) { }
 }
