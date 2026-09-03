92e2272232532c8b4d6ea0ed320d6b1756ed2f6d
Binozo <entwickler@binozoworks.de>
2026-09-03 11:10:24 +0200

    migrate(IIDRemovableBinaryIndex): Migrate interface default implementation to standalone implementation

--- src/FaissNET.Cpu/Interfaces/IIDRemovableBinaryIndex.cs
+++ src/FaissNET.Cpu/Interfaces/IIDRemovableBinaryIndex.cs
@@ -1,6 +1,6 @@
+using Faiss.Cpu.Selectors;
 using Faiss.Interop.Errors;
 using Faiss.Interop.NativeMethods;
-using Faiss.Search;
 
 namespace Faiss.Cpu.Interfaces;
 
@@ -11,12 +11,17 @@ public interface IIDRemovableBinaryIndex : INativeBinaryIndex, IBinaryIndex
     /// </summary>
     /// <param name="selector">The selector containing the IDs to drop.</param>
     /// <returns>The number of vectors successfully removed.</returns>
-    public long RemoveIds(IIDSelector selector)
+    public long RemoveIds(IDSelector selector);
+}
+
+internal static class IDRemovableBinaryIndexImpl 
+{
+    public static long RemoveIds(INativeBinaryIndex index, IDSelector selector)
     {
         ArgumentNullException.ThrowIfNull(selector);
 
         FaissErrorHandler.ThrowIfError(
-            Native.faiss_IndexBinary_remove_ids(Handle, selector.ToNative(), out nuint removedCount)
+            Native.faiss_IndexBinary_remove_ids(index.Handle, selector.SafeHandle, out nuint removedCount)
         );
 
         return (long)removedCount;
