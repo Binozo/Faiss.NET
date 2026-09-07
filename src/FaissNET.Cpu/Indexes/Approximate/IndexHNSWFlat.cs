fcb30f37c117a86bbb27f07d5515bb59c6584a0f
Binozo <entwickler@binozoworks.de>
2026-09-07 11:41:45 +0200

    migrate(IndexHNSWFlat): Completed migration to new implementations

--- src/FaissNET.Cpu/Indexes/Approximate/IndexHNSWFlat.cs
+++ src/FaissNET.Cpu/Indexes/Approximate/IndexHNSWFlat.cs
@@ -6,7 +6,7 @@ using Faiss.Models;
 namespace Faiss.Cpu.Indexes.Approximate;
 
 /// <inheritdoc/>
-public class IndexHNSWFlat : IndexHNSW, IFlatIndex, IClonableFloatIndex<IndexHNSWFlat>, IFromNativeIndexHandle<IndexHNSWFlat>
+public class IndexHNSWFlat : IndexHNSW, IClonableFloatIndex<IndexHNSWFlat>, IFromNativeIndexHandle<IndexHNSWFlat>
 {
     public IndexHNSWFlat(int dimensions, int m = 32, MetricType metricType = MetricType.L2) : this($"HNSW{m},Flat", dimensions, metricType)
     {
@@ -22,10 +22,7 @@ public class IndexHNSWFlat : IndexHNSW, IFlatIndex, IClonableFloatIndex<IndexHNS
 
     static IndexHNSWFlat IFromNativeIndexHandle<IndexHNSWFlat>.FromHandle(FaissIndexHandle handle) => new(handle);
     
-    private static FaissIndexHandle CreateHandle(string description, int dimensions, MetricType metricType)
-    {
-        return IndexFactory.Create<IndexHNSWFlat>(description, dimensions, metricType).NativeHandle;
-    }
+    private static FaissIndexHandle CreateHandle(string description, int dimensions, MetricType metricType) => IndexFactory.Create<IndexHNSWFlat>(description, dimensions, metricType).NativeHandle;
 
-    public override IndexHNSWFlat Clone() => ((IClonableFloatIndex<IndexHNSWFlat>)this).Clone();
+    public override IndexHNSWFlat Clone() => ClonableFloatIndexImpl<IndexHNSWFlat>.Clone(this);
 }
