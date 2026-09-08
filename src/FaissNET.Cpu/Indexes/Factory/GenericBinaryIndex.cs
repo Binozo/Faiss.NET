5fafa963b1faf67da18a6ffa5269fe8232b4f7a1
Binozo <entwickler@binozoworks.de>
2026-09-08 11:48:50 +0200

    migrate(GenericBinaryIndex): Completed migration to new implementations

--- src/FaissNET.Cpu/Indexes/Factory/GenericBinaryIndex.cs
+++ src/FaissNET.Cpu/Indexes/Factory/GenericBinaryIndex.cs
@@ -1,9 +1,9 @@
 using Faiss.Cpu.Indexes.Binary;
 using Faiss.Cpu.Interfaces;
+using Faiss.Cpu.Search.Parameters;
 using Faiss.Cpu.Search.Range;
-using Faiss.Interfaces;
+using Faiss.Cpu.Selectors;
 using Faiss.Interop.SafeHandles;
-using Faiss.Search;
 
 namespace Faiss.Cpu.Indexes.Factory;
 
@@ -13,23 +13,23 @@ public sealed class GenericBinaryIndex : BinaryIndex, ITrainableBinaryIndex, IID
     {
     }
 
-    public bool IsTrained => ((ITrainableBinaryIndex)this).IsTrained;
+    public bool IsTrained => TrainableBinaryIndexImpl.IsTrained(this);
 
-    public Task TrainAsync(long count, ReadOnlyMemory<byte> vectors) => ((ITrainableBinaryIndex)this).TrainAsync(count, vectors);
+    public Task TrainAsync(long count, ReadOnlyMemory<byte> vectors) => TrainableBinaryIndexImpl.TrainAsync(this, count, vectors);
 
-    public void Add(long count, ReadOnlySpan<byte> vectors) => ((IIDSequentialBinaryIndex)this).Add(count, vectors);
+    public void Add(long count, ReadOnlySpan<byte> vectors) => IDSequentialBinaryIndexImpl.Add(this, count, vectors);
 
-    public void Add(long count, ReadOnlySpan<byte> vectors, ReadOnlySpan<long> xids) => ((IIDMappedBinaryIndex)this).Add(count, vectors, xids);
+    public void Add(long count, ReadOnlySpan<byte> vectors, ReadOnlySpan<long> xids) => IDMappedBinaryIndexImpl.Add(this, count, vectors, xids);
 
-    public long RemoveIds(IIDSelector selector) => ((IIDRemovableBinaryIndex)this).RemoveIds(selector);
+    public long RemoveIds(IDSelector selector) => IDRemovableBinaryIndexImpl.RemoveIds(this, selector);
 
-    public void RangeSearch(long count, ReadOnlySpan<byte> queryVectors, byte radius, RangeSearchResult result) => ((IRangeSearchBinaryIndex)this).RangeSearch(count, queryVectors, radius, result);
+    public void RangeSearch(long count, ReadOnlySpan<byte> queryVectors, byte radius, RangeSearchResult result) => RangeSearchBinaryIndexImpl.RangeSearch(this, count, queryVectors, radius, result);
 
-    public void SearchWithParams(long count, ReadOnlySpan<byte> queryVectors, int k, ISearchParameters parameters, Span<int> distances, Span<long> labels) => ((IParamsBinarySearchIndex)this).SearchWithParams(count, queryVectors, k, parameters, distances, labels);
+    public void SearchWithParams(long count, ReadOnlySpan<byte> queryVectors, int k, SearchParameters parameters, Span<int> distances, Span<long> labels) => ParamsBinarySearchIndexImpl.SearchWithParams(this, count, queryVectors, k, parameters, distances, labels);
 
-    public byte[] Reconstruct(long key) => ((IReconstructBinaryIndex)this).Reconstruct(key);
+    public byte[] Reconstruct(long key) => ReconstructBinaryIndexImpl.Reconstruct(this, key);
 
-    public byte[] Reconstruct(long startKey, long count) => ((IReconstructBinaryIndex)this).Reconstruct(startKey, count);
+    public byte[] Reconstruct(long startKey, long count) => ReconstructBinaryIndexImpl.Reconstruct(this, startKey, count);
 
     static GenericBinaryIndex IFromNativeBinaryIndexHandle<GenericBinaryIndex>.FromHandle(FaissBinaryIndexHandle handle) => new(handle);
 }
