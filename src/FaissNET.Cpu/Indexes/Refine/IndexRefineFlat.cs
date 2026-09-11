cb2a66f8e3e3c2a05c997b9e93f742e204c7335d
Binozo <entwickler@binozoworks.de>
2026-09-11 12:13:37 +0200

    fix(IndexRefineFlat): Improve base index handling

--- src/FaissNET.Cpu/Indexes/Refine/IndexRefineFlat.cs
+++ src/FaissNET.Cpu/Indexes/Refine/IndexRefineFlat.cs
@@ -19,8 +19,9 @@ internal readonly struct IndexRefineFlatRelease : IFaissRelease
 public sealed class IndexRefineFlat<T> : FloatIndex, ITrainableFloatIndex, IIDSequentialFloatIndex, IRangeSearchFloatIndex, IReconstructFloatIndex, IComputeResidualFloatIndex, ICodeFloatIndex, ICpuFloatIndex, ISerializableFloatIndex, IFromNativeIndexHandle<IndexRefineFlat<T>>, IClonableFloatIndex<IndexRefineFlat<T>> where T : FloatIndex, IIDSequentialFloatIndex, IFromNativeIndexHandle<T>
 {
     public readonly T BaseIndex;
+    private readonly bool _ownBaseIndex;
 
-    public IndexRefineFlat(T baseIndex, bool ownBaseIndex = true) : this(CreateHandle(baseIndex), ownBaseIndex)
+    public IndexRefineFlat(T baseIndex, bool ownBaseIndex = true) : this(CreateHandle(baseIndex), baseIndex, ownBaseIndex)
     {
         if (ownBaseIndex)
         {
@@ -28,10 +29,11 @@ public sealed class IndexRefineFlat<T> : FloatIndex, ITrainableFloatIndex, IIDSe
         }
     }
     
-    internal IndexRefineFlat(FaissIndexHandle handle, bool ownBaseIndex = true) : base(handle)
+    internal IndexRefineFlat(FaissIndexHandle handle, T? baseIndex = null, bool ownBaseIndex = true) : base(handle)
     {
+        BaseIndex = baseIndex ?? T.FromPointer(Native.faiss_IndexRefineFlat_base_index(NativeHandle));
         OwnBaseIndex = ownBaseIndex;
-        BaseIndex = T.FromPointer(Native.faiss_IndexRefineFlat_base_index(NativeHandle), false);
+        _ownBaseIndex = ownBaseIndex;
     }
 
     public bool OwnBaseIndex
@@ -40,27 +42,27 @@ public sealed class IndexRefineFlat<T> : FloatIndex, ITrainableFloatIndex, IIDSe
         private set => Native.faiss_IndexRefineFlat_set_own_fields(NativeHandle, value);
     }
 
-    public bool IsTrained => ((ITrainableFloatIndex)this).IsTrained;
+    public bool IsTrained => TrainableFloatIndexImpl.IsTrained(this);
 
-    public Task TrainAsync(long count, ReadOnlyMemory<float> vectors) => ((ITrainableFloatIndex)this).TrainAsync(count, vectors);
+    public Task TrainAsync(long count, ReadOnlyMemory<float> vectors) => TrainableFloatIndexImpl.TrainAsync(this, count, vectors);
 
-    public void Add(long count, ReadOnlySpan<float> vectors) => ((IIDSequentialFloatIndex)this).Add(count, vectors);
+    public void Add(long count, ReadOnlySpan<float> vectors) => IDSequentialFloatIndexImpl.Add(this, count, vectors);
 
-    public void RangeSearch(long count, ReadOnlySpan<float> queryVectors, float radius, RangeSearchResult result) => ((IRangeSearchFloatIndex)this).RangeSearch(count, queryVectors, radius, result);
+    public void RangeSearch(long count, ReadOnlySpan<float> queryVectors, float radius, RangeSearchResult result) => RangeSearchFloatIndexImpl.RangeSearch(this, count, queryVectors, radius, result);
 
-    public float[] Reconstruct(long key) => ((IReconstructFloatIndex)this).Reconstruct(key);
+    public float[] Reconstruct(long key) => ReconstructFloatIndexImpl.Reconstruct(this, key);
 
-    public float[] Reconstruct(long startKey, long count) => ((IReconstructFloatIndex)this).Reconstruct(startKey, count);
+    public float[] Reconstruct(long startKey, long count) => ReconstructFloatIndexImpl.Reconstruct(this, startKey, count);
 
-    public void ComputeResidual(ReadOnlySpan<float> originalVector, Span<float> residualVector, long key) => ((IComputeResidualFloatIndex)this).ComputeResidual(originalVector, residualVector, key);
+    public void ComputeResidual(ReadOnlySpan<float> originalVector, Span<float> residualVector, long key) => ComputeResidualFloatIndexImpl.ComputeResidual(this, originalVector, residualVector, key);
 
-    public void ComputeResidual(ReadOnlySpan<float> originalVectors, Span<float> residualVectors, ReadOnlySpan<long> keys) => ((IComputeResidualFloatIndex)this).ComputeResidual(originalVectors, residualVectors, keys);
+    public void ComputeResidual(ReadOnlySpan<float> originalVectors, Span<float> residualVectors, ReadOnlySpan<long> keys) => ComputeResidualFloatIndexImpl.ComputeResidual(this, originalVectors, residualVectors, keys);
 
-    public long GetStandaloneCodeSize() => ((ICodeFloatIndex)this).GetStandaloneCodeSize();
+    public long GetStandaloneCodeSize() => CodeFloatIndexImpl.GetStandaloneCodeSize(this);
 
-    public void Encode(long count, ReadOnlySpan<float> vectors, Span<byte> outputBytes) => ((ICodeFloatIndex)this).Encode(count, vectors, outputBytes);
+    public void Encode(long count, ReadOnlySpan<float> vectors, Span<byte> outputBytes) => CodeFloatIndexImpl.Encode(this, count, vectors, outputBytes);
 
-    public void Decode(long count, ReadOnlySpan<byte> inputBytes, Span<float> outputVectors) => ((ICodeFloatIndex)this).Decode(count, inputBytes, outputVectors);
+    public void Decode(long count, ReadOnlySpan<byte> inputBytes, Span<float> outputVectors) => CodeFloatIndexImpl.Decode(this, count, inputBytes, outputVectors);
 
     /// <summary>
     /// Oversampling factor for refinement. Must be >= 1.
@@ -81,20 +83,32 @@ public sealed class IndexRefineFlat<T> : FloatIndex, ITrainableFloatIndex, IIDSe
     private static FaissIndexHandle CreateHandle(T baseIndex)
     {
         if (baseIndex.Metric != MetricType.L2 && baseIndex.Metric != MetricType.InnerProduct)
-        {
             throw new ArgumentOutOfRangeException(nameof(baseIndex), "Metric must be L2 or InnerProduct");
-        }
 
         if (baseIndex.TotalCount != 0)
-        {
             throw new FaissArgumentException($"{nameof(baseIndex)} must be empty");
-        }
 
         FaissErrorHandler.ThrowIfError(Native.faiss_IndexRefineFlat_new(out IntPtr ptr, baseIndex.Handle));
         return new FaissIndexHandle<IndexRefineFlatRelease>(ptr);
     }
+    
+    private static FaissIndexHandle Wrap(IntPtr handle, bool ownsHandle = true)
+        => new FaissIndexHandle<IndexRefineFlatRelease>(handle, ownsHandle);
+
+    static IndexRefineFlat<T> IFromNativeIndexHandle<IndexRefineFlat<T>>.FromPointer(IntPtr handle, bool ownsHandle)
+        => new(Wrap(handle, ownsHandle));
 
     static IndexRefineFlat<T> IFromNativeIndexHandle<IndexRefineFlat<T>>.FromHandle(FaissIndexHandle handle) => new(handle);
 
-    public IndexRefineFlat<T> Clone() => ((IClonableFloatIndex<IndexRefineFlat<T>>)this).Clone();
+    public IndexRefineFlat<T> Clone() => ClonableFloatIndexImpl<IndexRefineFlat<T>>.Clone(this);
+
+    public override void Dispose()
+    {
+        if (_ownBaseIndex)
+        {
+            BaseIndex.Dispose();
+        }
+
+        base.Dispose();
+    }
 }
